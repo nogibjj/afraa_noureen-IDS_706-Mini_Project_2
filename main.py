3b7ca47e93c98b724c116a231ab4ff32aefd7ad6
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-06 19:33:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def display_movies_stats():
     Display basic info about movies dataset
     """
     df = pd.read_csv("MoviesTopRated.csv")
-    return df.loc['0','vote_average']
+    return df.loc[0,'vote_average']
