89d03accdd1d0f47b5eed4644f03e9390be27f58
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-08 23:26:22 +0000

    updated main.py and test_main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """main module"""
 import pandas as pd
 
+
 def display_highest_votes():
     """
     Display basic info about movies dataset
     """
-    data_f = pd.read_csv("MoviesTopRated.csv")
-    new_df = data_f.query('vote_average == vote_average.max()')
-    return new_df
+    movies_df = pd.read_csv("MoviesTopRated.csv")
+    vote_df = movies_df.query("vote_average == vote_average.max()")
+    print(movies_df.describe())
+    print("\nDetails of the movies that were given the highest votes are: \n")
+    print(vote_df)
+    return vote_df
--- test_main.py
+++ test_main.py
@@ -1,15 +1,17 @@
 """test_main module"""
 import main
 
+
 def test_main():
-    '''
+    """
     testing function for main
-    '''
+    """
     highest_votes = main.display_highest_votes()
-    #print(highest_votes.loc[0,"vote_count"])
-    assert highest_votes.loc[0,"vote_count"] == 18448
-    #print(highest_votes.loc[1,"vote_count"])
-    assert highest_votes.loc[1,"vote_count"] == 24376
+    # print(highest_votes.loc[0,"vote_count"])
+    assert highest_votes.loc[0, "vote_count"] == 18448
+    # print(highest_votes.loc[1,"vote_count"])
+    assert highest_votes.loc[1, "vote_count"] == 24376
+
 
 if __name__ == "__main__":
     test_main()
