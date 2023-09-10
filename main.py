66bb8557bf05ab90dba7dc74709a576c1eec0a59
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-09-10 04:29:42 +0000

    new commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """main module"""
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def display_highest_votes():
     """
@@ -8,7 +8,11 @@ def display_highest_votes():
     """
     movies_df = pd.read_csv("MoviesTopRated.csv")
     vote_df = movies_df.query("vote_average == vote_average.max()")
+    print("Summary Statistics of the Top Rated Movies Dataset:\n")
     print(movies_df.describe())
+    plt.figure(figsize=(10,6))
+    plt.hist(movies_df["vote_average"], bins=10)
+    plt.show()
     print("\nDetails of the movies that were given the highest votes are: \n")
     print(vote_df)
     return vote_df
